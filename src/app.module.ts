71e01acda68a0328d5c34429d24b1dc4e5c4d308
D4nt11 <Profesor07@yandex.ru>
2024-11-12 22:34:49 +0300

    first request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DoctorModule } from './doctor/doctor.module';
 import { IndicatorsModule } from './indicators/indicators.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DoctorModule, IndicatorsModule],
+  imports: [DoctorModule, IndicatorsModule, UserModule],
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient()
 @Injectable()
-export class UserService {}
+export class UserService {
+    async findAll(){
+        const allUsers = await prisma.user.findMany()
+        console.log(allUsers)
+    }
+}
